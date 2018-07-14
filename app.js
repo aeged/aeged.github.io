975b138e6a80b85c4139c9d15f77e31eddb4621e
Alexander Gednov <ttrojan@usc.edu>
2018-07-14 10:48:52 -0700

    added button animation with jQuery

--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-const lightStylesheet = 'styles.css';
-const darkStylesheet = 'styles_dark.css';
-const toggleDarkMode = document.querySelector('button');
+const lightStylesheet = 'styles/styles.css';
+const darkStylesheet = 'styles/styles_dark.css';
+const toggleDarkMode = $('#toggle-dark-mode');
+// const toggleDarkMode = document.querySelector('#toggle-dark-mode');
 var isDarkModeOn = false; // dark mode is off by default
 
-toggleDarkMode.addEventListener('click', () => {
+// toggleDarkMode.addEventListener('click', () => {
+//     if (isDarkModeOn == false) {
+//         document.querySelector('#stylesheet').href = darkStylesheet;
+//         isDarkModeOn = true;
+//         console.log('Dark Mode is now on');
+//     }
+//     else {
+//         document.querySelector('#stylesheet').href = lightStylesheet;
+//         isDarkModeOn = false;
+//         console.log('Dark Mode is now off');
+//     }
+// });
+
+$('#toggle-dark-mode').click(function() {
     if (isDarkModeOn == false) {
-        document.querySelector('#stylesheet').href = darkStylesheet;
+        $('#dark-mode-icon').animate({  borderSpacing: 180 }, {
+            step: function(now,fx) {
+              $(this).css('-webkit-transform','rotate('+now+'deg)'); 
+              $(this).css('-moz-transform','rotate('+now+'deg)');
+              $(this).css('transform','rotate('+now+'deg)');
+            },
+            duration:'slow'
+        },'linear');
+
+        $('#stylesheet').attr('href', darkStylesheet);
         isDarkModeOn = true;
         console.log('Dark Mode is now on');
     }
     else {
-        document.querySelector('#stylesheet').href = lightStylesheet;
+        $('#dark-mode-icon').animate({  borderSpacing: -180 }, {
+            step: function(now,fx) {
+              $(this).css('-webkit-transform','rotate('+now+'deg)'); 
+              $(this).css('-moz-transform','rotate('+now+'deg)');
+              $(this).css('transform','rotate('+now+'deg)');
+            },
+            duration:'slow'
+        },'linear');
+
+        $('#stylesheet').attr('href', lightStylesheet);
         isDarkModeOn = false;
         console.log('Dark Mode is now off');
     }
-});
+})
